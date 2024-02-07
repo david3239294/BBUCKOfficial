bcea886eb971f2ec1bf42b68741a1426f5599f2b
unknown <david3239294@gmail.com>
2024-02-07 11:51:13 -0500

    Added mini game

--- pages/Game.tsx
+++ pages/Game.tsx
@@ -23,21 +23,19 @@ const Game: NextPage = () => {
   
       <div className="z-0 h-screen">
         <SplineObj
-          scene={"./spline/3_d_third_person_shooter_game"}
+          scene={"./spline/3_d_third_person_shooter_game.spline"} {/* Update the file extension if necessary */}
         />
       </div>
   
       <div className="absolute flex flex-col w-full gap-5 px-5 text-white h-fit lg:w-3/5 xl:w-1/2 left-2 sm:left-32 top-2 sm:top-10 z-100 sm:gap-6 sm:px-0">
         <div className={`ml-16 sm:ml-0 text-3xl md:text-4xl xl:text-5xl animated-heading bold md:mb-10 ${spaceBoards.className}`}>
-         Groks Laboratory is developing A Game?!?!
+          Groks Laboratory is developing A Game?!?!
   
           <div>
             <p className={tasaOrbiter.className}>
             </p>
   
             <p className="hidden w-full h-5 sm:block"></p>
-  
-           
           </div>
   
           <div className="absolute bottom-0 right-0 flex-col flex-grow hidden w-1/2 h-screen lg:flex flex-end">
