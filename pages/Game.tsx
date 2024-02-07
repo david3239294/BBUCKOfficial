13c175741ad414e91f5c8673c509e66c4853dd0f
unknown <david3239294@gmail.com>
2024-02-07 12:30:45 -0500

    Added mini game

--- pages/Game.tsx
+++ pages/Game.tsx
@@ -2,43 +2,52 @@ import Head from "next/head";
 import SplineObj from "@components/SplineObject";
 import useSound from "use-sound";
 import { NextPage } from "next";
-import { spaceBoards, tasaOrbiter } from "@font";
 import Canvas from "@components/Particle";
+import { spaceBoards, tasaOrbiter } from "@font";
 
-const Game: NextPage = () => {
+const Game : NextPage = () => {
   const snapSfx = "./sounds/snap.wav";
   const confirmSfx = "./sounds/confirm.wav";
   const [playSnap, { stop: stopSnap }] = useSound(snapSfx, { volume: 0.25 });
   const [playConfirm] = useSound(confirmSfx, { volume: 0.25 });
-
   return (
     <>
       <Head>
-        <title>MiniGame | GrokLabs</title>
+        <title>Mini-Game | GrokLabs</title>
         <meta
           name="description"
-          content="MiniGame | GrokLabs"
+          content="About | Groklabs"
         />
       </Head>
-
       <div className="z-0 h-screen">
-        <img src="./images/game.png" alt="Game Image" className="object-cover w-full h-full" />
+        <SplineObj
+         
+         scene={"./spline/3_d_game.spline"}
+        />
       </div>
-
       <div className="absolute flex flex-col w-full gap-5 px-5 text-white h-fit lg:w-3/5 xl:w-1/2 left-2 sm:left-32 top-2 sm:top-10 z-100 sm:gap-6 sm:px-0">
-        <div className={`ml-16 sm:ml-0 text-3xl md:text-4xl xl:text-5xl animated-heading bold md:mb-10 ${spaceBoards.className}`}>
-          Groks Laboratory is developing A Game?!?!
-
-          <div>
-            <p className={tasaOrbiter.className}></p>
-
-            <p className="hidden w-full h-5 sm:block"></p>
-          </div>
-
-          <div className="absolute bottom-0 right-0 flex-col flex-grow hidden w-1/2 h-screen lg:flex flex-end">
-            <Canvas />
-          </div>
+        <div
+          className={`ml-16 sm:ml-0 text-3xl md:text-4xl xl:text-5xl animated-heading bold md:mb-10 ${spaceBoards.className}`}
+        >
+   Groks Laboratory is developing a game???!!
         </div>
+      
+        
+        <p className="hidden w-full h-5 sm:block"></p>
+        <a href="https://t.me/GrokLabsOfficial" target="_blank">
+          <button
+            className={`resume-button h-fit w-fit px-5 py-2 text-md text-accent shadow-md font-semibold mb-3 ${spaceBoards.className}`}
+            onMouseEnter={() => playSnap()}
+            onMouseLeave={() => stopSnap()}
+            onClick={() => playConfirm()}
+          >
+            Join Telegram Here
+          </button>
+        </a>
+      </div>
+    
+      <div className="absolute bottom-0 right-0 flex-col flex-grow hidden w-1/2 h-screen lg:flex flex-end">
+        <Canvas />
       </div>
     </>
   );
